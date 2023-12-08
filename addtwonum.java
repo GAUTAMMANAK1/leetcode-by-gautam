22b816ea032afeda4483890cdc3ffa7e47460273
gautammanak <gautammanak1@gmail.com>
2023-12-09 02:48:14 +0530

    day 1

--- addtwonum.java
+++ addtwonum.java
@@ -2,9 +2,23 @@ import java.util.*;
 public class Main {
     public static void main (String [] args )
     {
-        int x = 10;
-        int y = 15;
+        // Adding
+        int x = 15;
+        int y = 10;
         int sum = x+y;
-        System.out.print(sum);
+        System.out.println("Adding="+ sum);
+        //multiple
+        int multi = x*y;
+        System.out.println("Multiple="+multi);
+        // Divide
+        int v1 =x/y;
+        int v2 = y/x;
+        int v3 = x%y;
+        System.out.println (v1);
+                System.out.println (v2);
+                        System.out.println (v3);
+                        //Bodmass
+                        int exp = (x*y) / (x+y);
+                        System.out.println ("Bodmass=" + exp);
     }
 }

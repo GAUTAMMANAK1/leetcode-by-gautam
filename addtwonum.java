56eb72b9f24ad8f53a6a2cd9b52a1a276644a509
gautammanak <gautammanak1@gmail.com>
2023-12-09 02:20:21 +0530

    day 1

---
+++ addtwonum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main (String [] args )
+    {
+        int x = 10;
+        int y = 15;
+        int sum = x+y;
+        System.out.print(sum);
+    }
+}

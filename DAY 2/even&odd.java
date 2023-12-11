c4daf7dfed54e730835b450bf85042b2f78f77a3
gautammanak <gautammanak1@gmail.com>
2023-12-12 02:01:26 +0530

    day 2

---
+++ DAY 2/even&odd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        int x=11;
+        // int X=10; (out put is odd)
+        if (x%2==0)
+        {
+            System.out.println(x+"is odd.");
+            
+        }
+    
+        else{
+            System.out.println(x+"is even.");
+            
+        }
+    }
+    
+}

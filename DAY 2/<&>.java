c12f0844dda5b628b83a1d4d0123e2c1162e5e52
gautammanak <gautammanak1@gmail.com>
2023-12-12 16:04:31 +0530

    Co-authored-by: gautammanak <gautammanak1@gmail.com> Co-authored-by: GAUTAM MANAK <106014185+GAUTAMMANAK1@users.noreply.github.com>

---
+++ DAY 2/<&>.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        int n1=10;
+        int n2=11;
+        if (n1==n2)
+        {
+            System.out.println(n1+"is equal to "+n2);
+            
+        }
+    
+        else{
+            System.out.println(n1+"is not equal to "+n2);
+            
+        }
+    }
+    
+}
